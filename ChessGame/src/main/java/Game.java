e899e21adfa9cc6f25954add95cb61dbdc51a758
Tony Pun <tpun27@gmail.com>
2017-06-08 17:45:55 -0400

    Create startGameLoop() to play a Game

--- ChessGame/src/main/java/Game.java
+++ ChessGame/src/main/java/Game.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 /**
  * Class for Chess Game
  */
@@ -8,6 +9,48 @@ public class Game {
     public Game() {
         chessBoard = new Grid();
         chessBoard.initializeBoardPieces();
+    }
+
+    public void startGameLoop() {
+        char inputChar, castleChar;
+        String initialCoordinate, finalCoordinate;
+
+
+        inputChar = '?';
+        while (inputChar != 'Q') {
+            inputChar = getMove();
+
+            switch (inputChar) {
+                case 'M':
+                    System.out.println("Enter The Current Coordinate Of The Piece You Wish To Move");
+                    initialCoordinate = getInputCoordinate();
+                    System.out.println("Enter The Destination Coordinate Of The Piece You Wish To Move");
+                    finalCoordinate = getInputCoordinate();
+                    makeMove(initialCoordinate, finalCoordinate);
+                    break;
+                case 'C':
+                    castleChar = getCastleChar();
+                    if (castleChar == 'K') {
+                        makeMove("O-O");
+                    }
+                    else {
+                        makeMove("O-O-O");
+                    }
+                    break;
+                case 'P':
+                    chessBoard.printBoard();
+                    break;
+                case 'I':
+                    printInstructions();
+                    break;
+                case 'Q':
+                    System.out.println("Thanks for playing!");
+                    break;
+                default:
+                    System.out.println("Invalid Input!");
+                    break;
+            }
+        }
     }
 
     public void makeMove(String initialPos, String newPos) {
@@ -30,5 +73,75 @@ public class Game {
 
     public void printBoard() {
         chessBoard.printBoard();
+    }
+
+    protected char getMove() {
+        char optionChar;
+        Scanner moveScanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("What Would You Like To Do?");
+            printInstructions();
+            optionChar = moveScanner.next().charAt(0);
+            if (optionChar == 'M' || optionChar == 'C' || optionChar == 'P' || optionChar == 'I' || optionChar == 'Q') {
+                break;
+            }
+        }
+        return optionChar;
+    }
+
+    protected String getInputCoordinate() {
+        String moveString;
+        char tempChar;
+        Scanner inputScanner = new Scanner(System.in);
+        boolean inputValid;
+
+        moveString = "";
+        inputValid = false;
+
+        while (!inputValid) {
+            System.out.println("Enter a 2-character Chess Coordinate");
+            moveString = inputScanner.nextLine();
+            if (moveString.length() != 2) {
+                System.out.println("Invalid Coordinate Length");
+                continue;
+            }
+
+            tempChar = moveString.charAt(0);
+            if (tempChar < 'a' || tempChar > 'h') {
+                System.out.println("Invalid First Character");
+                continue;
+            }
+
+            tempChar = moveString.charAt(1);
+            if (tempChar < '1' || tempChar > '8') {
+                System.out.println("Invalid Second Character");
+                continue;
+            }
+            inputValid = true;
+        }
+        return moveString;
+    }
+
+    protected char getCastleChar() {
+        char optionChar;
+        Scanner moveScanner = new Scanner(System.in);
+
+        while (true) {
+            System.out.println("Enter 'K' to castle King-side or 'Q' to castle Queen-side");
+            optionChar = moveScanner.next().charAt(0);
+            if (optionChar == 'K' || optionChar == 'Q') {
+                break;
+            }
+        }
+        return optionChar;
+    }
+
+    protected void printInstructions() {
+        System.out.println("Your options are: 'M', 'C', 'P', 'I', and 'Q'");
+        System.out.println("For Move, Castle, Print Board, Display Instructions and Quit\n");
+        System.out.println("All moves must use 2-character Chess Coordinates");
+        System.out.println("The first character is a letter from a to h");
+        System.out.println("The second character is an integer from 1 to 8:");
     }
 }
--- ChessGame/src/main/java/PlayChess.java
+++ ChessGame/src/main/java/PlayChess.java
@@ -3,27 +3,7 @@
  */
 public class PlayChess {
     public static void main(String args[]) {
-        // Supposed to simulate http://www.chessgames.com/perl/chessgame?gid=1106430
-
-        // Non-Professional Match
-        // https://www.chess.com/forum/view/game-analysis/shortest-checkmated-game-with-pawn-promotion (Post #14)
-        // includes pawn promotion
         Game chessGame = new Game();
-        chessGame.makeMove("d2", "d3");
-        chessGame.makeMove("e7", "e5");
-
-        chessGame.makeMove("b2", "b3");
-        chessGame.makeMove("e5", "e4");
-
-        chessGame.makeMove("e1", "d2"); // Black King in check
-        chessGame.makeMove("e4", "d3");
-
-        chessGame.makeMove("d2", "c3");
-        chessGame.makeMove("d3", "e2");
-
-        chessGame.makeMove("c3", "b2"); // Black King in check
-        chessGame.makeMove("e2", "d1"); // promote Pawn to Knight to checkmate Black King
-
-        chessGame.printBoard();
+        chessGame.startGameLoop();
     }
 }
